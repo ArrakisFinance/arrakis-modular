26e849387da9e090e6cb38f1b1366a05bcffab1a
Gevarist <gevariste@protonmail.com>
2023-10-03 15:19:34 +0530

    feat: add whitelisted modules

--- src/ArrakisMetaVaultV2.sol
+++ src/ArrakisMetaVaultV2.sol
@@ -3,11 +3,19 @@ pragma solidity 0.8.20;
 
 import {IArrakisMetaVaultV2} from "./interfaces/IArrakisMetaVaultV2.sol";
 import {ArrakisMetaVault, IArrakisLPModuleVault, CallFailed} from "./ArrakisMetaVault.sol";
+import {EnumerableSet} from "openzeppelin-contracts/contracts/utils/structs/EnumerableSet.sol";
 
 error SameModule();
 error ModuleNotEmpty(uint256 amount0, uint256 amount1);
+error AlreadyWhitelisted(address module);
+error NotWhitelistedModule(address module);
+error ActiveModule();
 
 contract ArrakisMetaVaultV2 is IArrakisMetaVaultV2, ArrakisMetaVault {
+    using EnumerableSet for EnumerableSet.AddressSet;
+
+    EnumerableSet.AddressSet internal _whitelistedModules;
+
     constructor(
         address token0_,
         address token1_,
@@ -17,11 +25,17 @@ contract ArrakisMetaVaultV2 is IArrakisMetaVaultV2, ArrakisMetaVault {
         address module_
     ) ArrakisMetaVault(token0_, token1_, owner_, init0_, init1_, module_) {}
 
-    function setModule(address module_, bytes[] calldata payloads_) external onlyManager {
-        if(address(module) == module_) revert SameModule();
+    function setModule(
+        address module_,
+        bytes[] calldata payloads_
+    ) external onlyManager {
+        if (address(module) == module_) revert SameModule();
+        if (!_whitelistedModules.contains(module_))
+            revert NotWhitelistedModule(module_);
 
         (uint256 amount0, uint256 amount1) = module.totalUnderlying();
-        if (amount0 != 0 || amount1 != 0) revert ModuleNotEmpty(amount0, amount1);
+        if (amount0 != 0 || amount1 != 0)
+            revert ModuleNotEmpty(amount0, amount1);
 
         module = IArrakisLPModuleVault(module_);
 
@@ -35,4 +49,31 @@ contract ArrakisMetaVaultV2 is IArrakisMetaVaultV2, ArrakisMetaVault {
         emit LogSetModule(module_, payloads_);
     }
 
+    function whitelistModules(address[] calldata modules_) external onlyOwner {
+        uint256 len = modules_.length;
+        for(uint256 i; i<len; i++) {
+            if(_whitelistedModules.contains(modules_[i]))
+                revert NotWhitelistedModule(modules_[i]);
+            _whitelistedModules.add(modules_[i]);
+        }
+
+        emit LogWhiteListedModules(modules_);
+    }
+
+    function blacklistModules(address[] calldata modules_) external onlyOwner {
+        uint256 len = modules_.length;
+        for(uint256 i; i<len; i++) {
+            if(!_whitelistedModules.contains(modules_[i]))
+                revert AlreadyWhitelisted(modules_[i]);
+            if(address(module) == modules_[i])
+                revert ActiveModule();
+            _whitelistedModules.remove(modules_[i]);
+        }
+
+        emit LogBlackListedModules(modules_);
+    }
+
+    function whitelistedModules() external view returns(address[] memory modules) {
+        return _whitelistedModules.values();
+    }
 }
--- src/interfaces/IArrakisMetaVaultV2.sol
+++ src/interfaces/IArrakisMetaVaultV2.sol
@@ -5,6 +5,8 @@ interface IArrakisMetaVaultV2 {
     // #region events.
 
     event LogSetModule(address module, bytes[] payloads_);
+    event LogWhiteListedModules(address[] modules_);
+    event LogBlackListedModules(address[] modules_);
 
     // #endregion events.
 
@@ -12,5 +14,17 @@ interface IArrakisMetaVaultV2 {
     /// @param module_ address of the new module
     /// @param payloads_ datas to initialize/rebalance on the new module
     function setModule(address module_, bytes[] calldata payloads_) external;
+
+    /// @notice function used to whitelist modules that can used by manager.
+    /// @param modules_ array of module addresses to be whitelisted.
+    function whitelistModules(address[] calldata modules_) external;
+
+    /// @notice function used to blacklist modules that can used by manager.
+    /// @param modules_ array of module addresses to be blacklisted.
+    function blacklistModules(address[] calldata modules_) external;
+
+    /// @notice function used to get the list of modules whitelisted.
+    /// @return modules whitelisted modules addresses.
+    function whitelistedModules() external view returns(address[] memory modules);
 }
 
