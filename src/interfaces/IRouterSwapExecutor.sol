56367baf7077924e2c3fec318b458131d6a30a9d
Gevarist <98388264+Gevarist@users.noreply.github.com>
2024-01-25 01:15:31 +0530

    fix: router payable

--- src/interfaces/IRouterSwapExecutor.sol
+++ src/interfaces/IRouterSwapExecutor.sol
@@ -4,7 +4,19 @@ pragma solidity ^0.8.20;
 import {SwapAndAddData} from "../structs/SRouter.sol";
 
 interface IRouterSwapExecutor {
+
+    // #region errors.
+
+    error OnlyRouter(address caller, address router);
+    error AddressZero();
+    error SwapCallFailed();
+    error ReceivedBelowMinimum();
+
+    // #endregion errors.
+
+
     function swap(SwapAndAddData memory _swapData)
         external
+        payable
         returns (uint256 amount0Diff, uint256 amount1Diff);
 }
