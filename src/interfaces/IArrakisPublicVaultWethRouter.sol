df20a6e01db05f20e745d8847ecbd1b75cc6989b
Gevarist <gevariste@protonmail.com>
2024-02-21 13:14:10 +0100

    chore: update IArrakisPublicVaultWethRouter

--- src/interfaces/IArrakisPublicVaultWethRouter.sol
+++ src/interfaces/IArrakisPublicVaultWethRouter.sol
@@ -1,7 +1,14 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.20;
 
-import {AddLiquidityData, SwapAndAddData} from "../structs/SRouter.sol";
+import {
+    AddLiquidityData,
+    SwapAndAddData,
+    RemoveLiquidityData,
+    AddLiquidityPermit2Data,
+    SwapAndAddPermit2Data,
+    RemoveLiquidityPermit2Data
+} from "../structs/SRouter.sol";
 
 interface IArrakisPublicVaultWethRouter {
 
@@ -16,4 +23,49 @@ interface IArrakisPublicVaultWethRouter {
     function wethAndAddLiquidity(
         AddLiquidityData memory params_
     ) external payable returns (uint256 amount0, uint256 amount1, uint256 sharesReceived);
+
+    function wethAndSwapAndAddLiquidity(
+        SwapAndAddData memory params_
+    )
+        external
+        payable
+        returns (
+            uint256 amount0,
+            uint256 amount1,
+            uint256 sharesReceived,
+            uint256 amount0Diff,
+            uint256 amount1Diff
+        );
+    
+    function removeLiquidityAndUnwrap(
+        RemoveLiquidityData memory params_
+    ) 
+        external
+        returns (uint256 amount0, uint256 amount1);
+
+    function wethAddLiquidityPermit2(
+        AddLiquidityPermit2Data memory params_
+    )
+        external
+        payable
+        returns (uint256 amount0, uint256 amount1, uint256 sharesReceived);
+
+    function wethSwapAndAddLiquidityPermit2(
+        SwapAndAddPermit2Data memory params_
+    )
+        external
+        payable
+        returns (
+            uint256 amount0,
+            uint256 amount1,
+            uint256 sharesReceived,
+            uint256 amount0Diff,
+            uint256 amount1Diff
+        );
+
+    function removeLiquidityPermit2AndUnwrap(
+         RemoveLiquidityPermit2Data memory params_
+    )
+        external
+        returns (uint256 amount0, uint256 amount1);
 }
