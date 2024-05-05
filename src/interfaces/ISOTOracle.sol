804cf57b8cd27190fce5525a3c53384d54259828
Gevarist <gevariste@protonmail.com>
2024-05-05 14:24:11 +0200

    chore: remove not implemented functions

--- src/interfaces/ISOTOracle.sol
+++ src/interfaces/ISOTOracle.sol
@@ -4,33 +4,6 @@ pragma solidity ^0.8.20;
 import {AggregatorV3Interface} from "./AggregatorV3Interface.sol";
 
 interface ISOTOracle {
-    // #region view functions.
-
-    function token0Decimals() external view returns (uint8);
-
-    function token1Decimals() external view returns (uint8);
-
-    function token0Base() external view returns (uint256);
-
-    function token1Base() external view returns (uint256);
-
-    function maxOracleUpdateDuration()
-        external
-        view
-        returns (uint32);
-
-    function feedToken0()
-        external
-        view
-        returns (AggregatorV3Interface);
-
-    function feedToken1()
-        external
-        view
-        returns (AggregatorV3Interface);
-
-    // #endregion view functions.
-
     function getSqrtOraclePriceX96()
         external
         view
