f2e7cf43a65f4970cac847e44f0aca09a1cf8a30
Gevarist <98388264+Gevarist@users.noreply.github.com>
2024-09-19 11:51:27 +0200

    test: bunker add test
    * test: bunker add t
    
    * chore: arrakis private hook test

--- script/mint.s.sol
+++ script/mint.s.sol
@@ -11,15 +11,16 @@ import {IArrakisMetaVaultPublic} from
 import {IArrakisPublicVaultRouter} from
     "../src/interfaces/IArrakisPublicVaultRouter.sol";
 import {TimeLock} from "../src/TimeLock.sol";
+import {AddLiquidityData} from "../src/structs/SRouter.sol";
 
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 // IMPORTANT !!! Fill in / check these sensitive varaibles before running script !!!
 
-address constant vault = 0x8cE9786dc4bbB558C1F219f10b1F2f70A6Ced7eC;
-address constant router = 0x0210771fE1D7aDC2a4F747e3553A5710132d2b36;
-uint256 constant maxAmount1 = 490_000;
-uint256 constant maxAmount0 = 160_000_000_000_000;
+address constant vault = 0xf790870ccF6aE66DdC69f68e6d05d446f1a6ad83;
+address constant router = 0x72aa2C8e6B14F30131081401Fa999fC964A66041;
+uint256 constant maxAmount0 = 98_000_000_000_000_000;
+uint256 constant maxAmount1 = 226_000_000;
 // !! your address below !!
 address constant receiver = 0x81a1e7F34b9bABf172087cF5df8A4DF6500e9d4d;
 
@@ -42,12 +43,21 @@ contract Mint is Script {
 
         address token0 = IArrakisMetaVault(vault).token0();
         address token1 = IArrakisMetaVault(vault).token1();
-        address module = address(IArrakisMetaVault(vault).module());
 
-        ERC20(token0).approve(module, amount0);
-        ERC20(token1).approve(module, amount1);
+        ERC20(token0).approve(router, amount0);
+        ERC20(token1).approve(router, amount1);
 
-        IArrakisMetaVaultPublic(vault).mint(shares, receiver);
+        IArrakisPublicVaultRouter(router).addLiquidity(
+            AddLiquidityData({
+                amount0Max: maxAmount0,
+                amount1Max: maxAmount1,
+                amount0Min: amount0,
+                amount1Min: amount1,
+                amountSharesMin: shares,
+                vault: vault,
+                receiver: receiver
+            })
+        );
 
         console.logString("Valantis Public Vault mint");
         console.logAddress(vault);
